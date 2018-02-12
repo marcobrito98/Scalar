5be849ec0dca10ec94ac283a98737553ce9feecf
Miguel Zilhao <mzilhao@gmail.com>
2018-02-12 20:24:09 +0900

    ScalarEvolve: add function that sets rhs of grid functions to zero
    as done in the Proca thorn

---
+++ ScalarEvolve/src/Scalar_zero_rhs.F90
@@ -0,0 +1,15 @@
+
+#include "cctk.h"
+
+subroutine Scalar_zero_rhs( CCTK_ARGUMENTS )
+
+  implicit none
+  DECLARE_CCTK_ARGUMENTS
+
+  rhs_phi1    = 0
+  rhs_phi2    = 0
+
+  rhs_Kphi1   = 0
+  rhs_Kphi2   = 0
+
+end subroutine Scalar_zero_rhs
