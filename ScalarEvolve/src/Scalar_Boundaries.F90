e93d0f37c1f19d90bb270ff2aae673d9ab321a11
Giuseppe Ficarra <gxfsma@COS-MATH2114.main.ad.rit.edu>
2023-02-25 15:22:50 -0500

    scalar/ScalarEvolve:: added boundary conditions for TmunuBase variables and ScalarEvolve energy fluxes which were not working okay at the refinement boundaries; at the moment this works only on Cartesian grids

--- ScalarEvolve/src/Scalar_Boundaries.F90
+++ ScalarEvolve/src/Scalar_Boundaries.F90
@@ -29,3 +29,40 @@ subroutine Scalar_Boundaries( CCTK_ARGUMENTS )
        call CCTK_WARN(0, "Failed to register BC for ScalarBase::Kphi!")
 
 end subroutine Scalar_Boundaries
+
+!=============================================================================
+!
+subroutine Scalar_calc_Tmunu_bdry( CCTK_ARGUMENTS )
+
+  implicit none
+  DECLARE_CCTK_ARGUMENTS
+  DECLARE_CCTK_PARAMETERS
+  DECLARE_CCTK_FUNCTIONS
+
+  CCTK_INT ierr
+  CCTK_INT, parameter :: one = 1
+  CCTK_INT bndsize
+
+  bndsize = 3
+
+  ierr = Boundary_SelectGroupForBC(cctkGH, CCTK_ALL_FACES, bndsize, -one, &
+       "TmunuBase::stress_energy_scalar", "flat")
+  if (ierr < 0)                                                           &
+       call CCTK_ERROR("Failed to register BC for TmunuBase::stress_energy_scalar!")
+
+  ierr = Boundary_SelectGroupForBC(cctkGH, CCTK_ALL_FACES, bndsize, -one, &
+       "TmunuBase::stress_energy_vector", "flat")
+  if (ierr < 0)                                                           &
+       call CCTK_ERROR("Failed to register BC for TmunuBase::stress_energy_vector!")
+
+  ierr = Boundary_SelectGroupForBC(cctkGH, CCTK_ALL_FACES, bndsize, -one, &
+       "TmunuBase::stress_energy_tensor", "flat")
+  if (ierr < 0)                                                           &
+       call CCTK_ERROR("Failed to register BC for TmunuBase::stress_energy_tensor!")
+
+  ierr = Boundary_SelectGroupForBC(cctkGH, CCTK_ALL_FACES, bndsize, -one, &
+       "ScalarEvolve::densities_SF", "flat")
+  if (ierr < 0)                                                           &
+       call CCTK_ERROR("Failed to register BC for ScalarEvolve::densities_SF!")
+
+end subroutine Scalar_calc_Tmunu_bdry
